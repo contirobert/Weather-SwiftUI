91cab09311afcfa4b7a7ae280afd0891cfa717d2
Bobby Conti <robert.conti@protonmail.com>
2019-06-10 20:58:17 -0400

    Removed CurrentWeather extension, UIImage no longer required

--- Weather/Model/CurrentWeather.swift
+++ Weather/Model/CurrentWeather.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 struct CurrentWeather: Codable {
     let temperature: Double
@@ -15,22 +14,4 @@ struct CurrentWeather: Codable {
     let precipProbability: Double
     let summary: String
     let icon: String
-}
-
-extension CurrentWeather {
-    var iconImage: UIImage {
-        switch icon {
-        case "clear-day": return #imageLiteral(resourceName: "clear-day")
-        case "clear-night": return #imageLiteral(resourceName: "clear-night")
-        case "rain": return #imageLiteral(resourceName: "rain")
-        case "snow": return #imageLiteral(resourceName: "snow")
-        case "sleet": return #imageLiteral(resourceName: "sleet")
-        case "wind": return #imageLiteral(resourceName: "wind")
-        case "fog": return #imageLiteral(resourceName: "fog")
-        case "cloudy": return #imageLiteral(resourceName: "cloudy")
-        case "partly-cloudy-day": return #imageLiteral(resourceName: "partly-cloudy-day")
-        case "partly-cloudy-night": return #imageLiteral(resourceName: "partly-cloudy-night")
-        default: return #imageLiteral(resourceName: "default")
-        }
-    }
 }
