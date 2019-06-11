a6f67d827c457dad28213c39379f4734b4268bd6
Bobby Conti <robert.conti@protonmail.com>
2019-06-10 20:13:54 -0400

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Bobby Conti on 6/10/19.
+//  Copyright © 2019 Bobby Conti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
